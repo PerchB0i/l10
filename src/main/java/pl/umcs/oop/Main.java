ba31a14ac02b66a52a4e850a1f26575e9bd361b2
KaminaPL <malytankus@gmail.com>
2024-05-16 14:00:44 +0200

    lab10

--- src/main/java/pl/umcs/oop/Main.java
+++ src/main/java/pl/umcs/oop/Main.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ImageProcessor image = new ImageProcessor();
-        image.loadImage("image.png");
-        image.setBrightness(100);
-        image.saveImage("image2.png");
+        ImageProcessor ip = new ImageProcessor();
+
+        try {
+            ip.loadImage("image.png");
+            ip.setBrightness(100);
+            ip.saveImage("image2.png");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
